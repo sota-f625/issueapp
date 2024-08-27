d3c899c95e1af01f3ba44da2554d2a5c8da460f5
sota-f625 <s7716xf625@gmail.com>
2024-08-27 16:57:02 +0900

    コントローラーの変更

--- src/main/java/in/techcamp/issueapp/IssueController.java
+++ src/main/java/in/techcamp/issueapp/IssueController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -34,5 +35,12 @@ public class IssueController {
         var issueList = issueRepository.findAll();
         model.addAttribute("issueList", issueList);
         return  "index";
+    }
+
+    @GetMapping("/issues/{id}")
+    public String issueDeail(@PathVariable long id, Model model){
+        var issue = issueRepository.findById(id);
+        model.addAttribute("issue", issue);
+        return "detail";
     }
 }
